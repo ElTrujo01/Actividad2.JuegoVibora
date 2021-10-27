11c7121b384ab6fe3d0e30a648350cc2b217be40
ElTrujo01 <A00827538@itesm.mx>
2021-10-27 10:03:00 -0500

    cambio de variables

--- code.py
+++ code.py
@@ -50,9 +50,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, bc) #cambio de variables
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, fc)
     update()
     ontimer(move, 100)
 
